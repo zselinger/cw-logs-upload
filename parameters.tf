4ec383bd71e71ef729cf990810faf55543b61ad8
zselinger <zselinger@gmail.com>
2022-11-17 21:24:27 +0100

    Added more parameters

--- parameters.tf
+++ parameters.tf
@@ -4,3 +4,31 @@ resource "aws_ssm_parameter" "efs_access_point_arn" {
   type        = "SecureString"
   value       = aws_efs_access_point.access_point_lambda.arn
 }
+
+resource "aws_ssm_parameter" "vpc_security_group" {
+  name        = "/cwlogs/vpc/security_group_name"
+  description = "Security group name"
+  type        = "SecureString"
+  value       = "sg-cf39e3a4"
+}
+
+resource "aws_ssm_parameter" "vpc_subnet_id1" {
+  name        = "/cwlogs/vpc/subnet_id1"
+  description = "Security group 1 name"
+  type        = "SecureString"
+  value       = "subnet-5b14f732"
+}
+
+resource "aws_ssm_parameter" "vpc_subnet_id2" {
+  name        = "/cwlogs/vpc/subnet_id2"
+  description = "Security group 2 name"
+  type        = "SecureString"
+  value       = "subnet-6c181514"
+}
+
+resource "aws_ssm_parameter" "vpc_subnet_id3" {
+  name        = "/cwlogs/vpc/subnet_id3"
+  description = "Security group 3 name"
+  type        = "SecureString"
+  value       = "subnet-fd99b5b7"
+}
--- src/workers/file_write_worker.js
+++ src/workers/file_write_worker.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 exports.handler = async () => {
   console.log('Hello from lambda');
 
-  fs.writeFileSync('/mnt/efs/test.txt', 'This is working with ssm');
+  fs.writeFileSync('/mnt/efs/test.txt', 'More parameters added');
 };
