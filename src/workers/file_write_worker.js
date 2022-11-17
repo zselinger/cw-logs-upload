b7acb4122bd767f6be08dbb47f3471e0d1b0fb11
zselinger <zselinger@gmail.com>
2022-11-17 17:03:54 +0100

    Inital terraform efs setup

--- src/workers/file_write_worker.js
+++ src/workers/file_write_worker.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 exports.handler = async () => {
   console.log('Hello from lambda');
 
-  fs.writeFileSync('/mnt/efs/test.txt', 'This is working of efs');
+  fs.writeFileSync('/mnt/efs/test.txt', 'This is working with terraform');
 };
