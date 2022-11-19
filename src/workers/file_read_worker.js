79657b2356f81225b0a9cf2da2dd00ece5fa0da2
zselinger <zselinger@gmail.com>
2022-11-19 21:47:21 +0100

    Testing cleanup

--- src/workers/file_read_worker.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-exports.handler = async () => {
-  console.log('Hello from lambda');
-
-  const data = fs.readFileSync('/mnt/efs/test.txt');
-  console.log(data.toString());
-};
--- src/workers/file_write_worker.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-exports.handler = async () => {
-  console.log('Hello from lambda');
-
-  fs.writeFileSync('/mnt/efs/test.txt', 'More parameters added');
-};
