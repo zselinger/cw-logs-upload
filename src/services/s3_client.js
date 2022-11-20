3e83f98138bb2e4147a2a4cd9e9169ca8a3e111b
zselinger <zselinger@gmail.com>
2022-11-20 10:12:32 +0100

    Added s3 client

---
+++ src/services/s3_client.js
@@ -0,0 +1,30 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+class S3 {
+  constructor() {
+    this.s3 = new S3Client({ region: 'eu-south-1' });
+  }
+
+  async uploadObject(Bucket, Key, Body) {
+    const bucketParams = {
+      Bucket,
+      Key,
+      Body,
+    };
+
+    try {
+      const data = await this.s3.send(new PutObjectCommand(bucketParams));
+      console.log(data);
+      console.log(
+        'Successfully uploaded object: ' +
+          bucketParams.Bucket +
+          '/' +
+          bucketParams.Key
+      );
+    } catch (err) {
+      console.log('Error', err);
+    }
+  }
+}
+
+module.exports = S3;
