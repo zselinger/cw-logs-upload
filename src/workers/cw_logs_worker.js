263222dd69914e7a6baa5599dbcb540965c7594b
zselinger <zselinger@gmail.com>
2022-11-22 21:14:49 +0100

    Final lambda setup

--- src/workers/cw_logs_worker.js
+++ src/workers/cw_logs_worker.js
@@ -1,10 +1,9 @@
 const CloudWatchClient = require('../services/cloudwatch_client');
 const S3Client = require('../services/s3_client');
+const fs = require('fs');
 const BUCKET = process.env.BUCKET;
 
 exports.handler = async () => {
-  console.log('Hello from lambda');
-
   const cloudwatch_client = new CloudWatchClient();
 
   const ret = await cloudwatch_client.getMetrics('Duration');
@@ -13,6 +12,7 @@ exports.handler = async () => {
   const s3_client = new S3Client();
 
   const file_name = new Date().toISOString().substring(0, 10);
-
   await s3_client.uploadObject(BUCKET, file_name, data.toString());
+
+  fs.writeFileSync(`/mnt/efs/${file_name}`, data.toString());
 };
