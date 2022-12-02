1e7ad8ed31b5daa1c32823ded5e0ae53038dd239
zselinger <zselinger@gmail.com>
2022-12-02 17:37:41 +0100

    Added ec2 with efs mounted automatically

--- main.tf
+++ main.tf
@@ -54,3 +54,94 @@ resource "aws_s3_bucket_acl" "private-acl" {
   bucket = aws_s3_bucket.metrics-bucket.id
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2_security_group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "EFS mount target"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "efs-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+
+  provisioner "local-exec" {
+    command = "echo  ${tls_private_key.my_key.private_key_pem} > mykey.pem"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "efs_instance" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t3.micro"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.ec2_security_group.name]
+}
+
+resource "null_resource" "configure_nfs" {
+  depends_on = [aws_instance.efs_instance]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.my_key.private_key_pem
+    host        = aws_instance.efs_instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir ~/efs-mount-point",
+      "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs_for_lambda.dns_name}:/  ~/efs-mount-point",
+      "sudo chmod go+rw ~/efs-mount-point",
+    ]
+  }
+}
